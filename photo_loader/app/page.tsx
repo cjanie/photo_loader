b4253c482065f2e230b7736f468cd7adaafcc257
cjanie <janiechun@hotmail.fr>
2023-12-18 01:24:07 +0100

    fetch request response

--- photo_loader/app/page.tsx
+++ photo_loader/app/page.tsx
@@ -1,6 +1,14 @@
 import Image from 'next/image'
+import { URL } from 'url';
 
 export default function Home() {
+
+
+  // https://medium.com/@boris.poehland.business/next-js-api-routes-how-to-read-files-from-directory-compatible-with-vercel-5fb5837694b9
+  const fetcher = () => fetch('url').then((res) => res.json());
+  const { data } = useSWR('api/readfiles', fetcher);
+
+
 
   const selectedPhotos: File[] = [];
 
@@ -15,7 +23,7 @@ export default function Home() {
         Persist Photos to&nbsp;
           <code className="font-mono font-bold">remote server</code>
         </button>
-        
+
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
@@ -37,14 +45,8 @@ export default function Home() {
       </div>
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+        {!data && "Loading..."}
+        {data && data.map((imgPath: string | undefined) => <img src={imgPath} alt="photo"/>)}
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -119,3 +121,9 @@ export default function Home() {
     </main>
   )
 }
+
+function useSWR(arg0: string, fetcher: () => Promise<any>): { data: any; } {
+  fetcher();
+  return {data: ""};
+}
+
