87bdeda9e8afdc79c708f94e25fd3a10a599cd42
cjanie <janiechun@hotmail.fr>
2024-02-09 20:52:58 +0100

    server page

---
+++ photo_loader/app/server/page.tsx
@@ -0,0 +1,121 @@
+import { writeFile } from "fs/promises"
+import Image from "next/image"
+import { join } from "path"
+
+// https://www.youtube.com/watch?v=-_bhH4MLq1Y
+
+export default function ServerUploadPage() {
+
+    async function upload(data:FormData) {
+        'use server'
+        const file: File | null = data.get('file') as unknown as File
+
+        if(!file) {
+            throw new Error('No file uploaded')
+        }
+
+        const bytes = await file.arrayBuffer()
+        const buffer = Buffer.from(bytes)
+
+        // With the file data in the buffer, do what you want
+        // Write it to the file system in a new location
+        const path = join('/', 'tmp', file.name)
+        await writeFile(path, buffer)
+        console.log('Open ' + path + ' to see the uploaded file')
+        return {
+            success: true
+        }
+
+
+
+    }
+
+    return (
+        
+
+<main className="flex min-h-screen flex-col items-center justify-between p-24">
+
+<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+  <form action={upload}>
+                <input type="file" name="file"/>
+                <input type="submit" value="Upload"/>
+            </form>
+
+</div>
+
+<div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+  
+</div>
+
+<div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
+  <a
+    href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <h2 className={`mb-3 text-2xl font-semibold`}>
+      Docs{' '}
+      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+        -&gt;
+      </span>
+    </h2>
+    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+      Find in-depth information about Next.js features and API.
+    </p>
+  </a>
+
+  <a
+    href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
+    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <h2 className={`mb-3 text-2xl font-semibold`}>
+      Learn{' '}
+      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+        -&gt;
+      </span>
+    </h2>
+    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+      Learn about Next.js in an interactive course with&nbsp;quizzes!
+    </p>
+  </a>
+
+  <a
+    href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <h2 className={`mb-3 text-2xl font-semibold`}>
+      Templates{' '}
+      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+        -&gt;
+      </span>
+    </h2>
+    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+      Explore starter templates for Next.js.
+    </p>
+  </a>
+
+  <a
+    href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+    className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    <h2 className={`mb-3 text-2xl font-semibold`}>
+      Deploy{' '}
+      <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+        -&gt;
+      </span>
+    </h2>
+    <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+      Instantly deploy your Next.js site to a shareable URL with Vercel.
+    </p>
+  </a>
+</div>
+</main>
+    );
+}
