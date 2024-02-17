a3fffebfefb7b60f7dd0bb565c6020fb74de5da0
cjanie <janiechun@hotmail.fr>
2024-02-17 23:51:47 +0100

    getAllFiles

--- photo_loader/app/page.tsx
+++ photo_loader/app/page.tsx
@@ -2,48 +2,37 @@
 import FileUploadComponant from './componants/FileUploadComponant'
 import FileDownloadComponant from './componants/FileDownloadComponant'
 import { di } from './di'
-import { fileNameQuery } from './firebase/db/fileNameQuery'
 import { storage } from './firebase/firebase-config'
-import { listAll, ref } from 'firebase/storage'
+import { ListResult, listAll, ref } from 'firebase/storage'
 import { useState } from 'react'
 
 export default function Home() {
 
   const [fileNames, setFileNames] = useState<string[]>([])
 
-  const filesStorage = () => {
+  const filesStorage = async () => {
 
-    var storedFilesNames: string[] = []
     // Create a reference under which you want to list
     const listRef = ref(storage, 'files');
     // Find all the prefixes and items.
-    listAll(listRef)
-    .then((listResult) => {
-    listResult.prefixes.forEach((folderRef) => {
-        // All the prefixes under listRef.
-        // You may call listAll() recursively on them.
-        console.log(folderRef.name + " full path = " + folderRef.fullPath)
-        storedFilesNames.push(folderRef.name)
-       
-    });
-    listResult.items.forEach((itemRef) => {
-        // All the items under listRef.
-        console.log(itemRef.name + " full path = " + itemRef.fullPath)
-    });
-    }).catch((error) => {
-    // Uh-oh, an error occurred!
-    console.error(error.message)
-    });
+    const listResult: ListResult = await listAll(listRef)
+
+    var storedFilesNames: string[] = []
+    listResult.items.forEach(storageReference => {
+      console.log(storageReference.name + " full path = " + storageReference.fullPath)
+      storedFilesNames.push(storageReference.name)
+    })
+    setFileNames(storedFilesNames)
   }
   filesStorage()
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+
         <FileUploadComponant uploadGateway={di.uploadGateway}/>
         {
-        
-          fileNameQuery.getFilesNames().map(fileName => <FileDownloadComponant downloadGateway={di.downloadGateway} fileName={fileName}/>)
+          fileNames.map(fileName => <FileDownloadComponant downloadGateway={di.downloadGateway} fileName={fileName}/>)
         }
         
       </div>
