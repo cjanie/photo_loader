47281662108bbb3b72cb6ddce76213bed79b8911
cjanie <janiechun@hotmail.fr>
2024-02-17 20:37:03 +0100

    remove unused

--- photo_loader/app/componants/FileDownloadComponant.tsx
+++ photo_loader/app/componants/FileDownloadComponant.tsx
@@ -9,48 +9,18 @@ interface FileDownload {
 }
 
 export default function FileDownloadComponant() {
-
-    const [items, setItems] = useState<string[]>()
-    const [error, setError] = useState<unknown | null>()
     
     const [downloadUrl, setDownLoadUrl] = useState<string>()
+
     
-    const path = `files/`;
-    const reference: StorageReference = ref(storage, path);
+    // const httpsReference = ref(storage, 'https://firebasestorage.googleapis.com/b/bucket/o/images%20stars.jpg')
 
-    useEffect(() => {
-        async () => {
-            try {
-
-                const result = await list(reference);
- 
-                const items = await Promise.all(
-                    result.items.map(async (item) => {
-                    const url = await getDownloadURL(item);
- 
-                    return url;
-                })
-          
-                );
-                setItems(items)
-
-            } catch (e: unknown) {
-                setError(e)
-            }
-
-        }
-    }, [reference, setItems, setError])
-
-    // Create a reference from an HTTPS URL
-// Note that in the URL, characters are URL escaped!
-// const httpsReference = ref(storage, 'https://firebasestorage.googleapis.com/b/bucket/o/images%20stars.jpg')
-
-const getUrl = () => {
-    //const httpsReference = ref(storage, 'https://firebasestorage.googleapis.com/v0/b/uploadtravelfile.appspot.com/o/files%2FBlondie.png?alt=media&token=e0a431c3-40b5-4f81-b144-d3248312d9f8')
-    getDownloadURL(ref(storage, 'files/Blondie.png')).then((url) => {
-        setDownLoadUrl(url)
-    })
-}
+    const getUrl = () => {
+        //const httpsReference = ref(storage, 'https://firebasestorage.googleapis.com/v0/b/uploadtravelfile.appspot.com/o/files%2FBlondie.png?alt=media&token=e0a431c3-40b5-4f81-b144-d3248312d9f8')
+        getDownloadURL(ref(storage, 'files/Blondie.png')).then((url) => {
+            setDownLoadUrl(url)
+        })
+    }
 
     return (
         <main className="flex min-h-screen flex-col items-center justify-between p-24">
@@ -65,13 +35,6 @@ const getUrl = () => {
                     )
                 }
             </div>
-    
-          <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-            <p>{items}</p>
-            <p>{items?.length}</p>
-            {error? <p>error</p>: null}
-          </div>
-          
         </main>
         )
 }
