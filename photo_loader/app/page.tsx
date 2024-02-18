2c85a6259a0e81261deee2c7b5252ec10248a18c
cjanie <janiechun@hotmail.fr>
2024-02-18 13:45:24 +0100

    menu

--- photo_loader/app/page.tsx
+++ photo_loader/app/page.tsx
@@ -12,7 +12,7 @@ interface UseCase {
 
 export default function Home() {
 
-  const [useCase, setUseCase] = useState<UseCase>()
+  const [useCase, setUseCase] = useState<UseCase | undefined>()
 
   const [fileNames, setFileNames] = useState<string[]>([])
 
@@ -47,18 +47,32 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+        {
+          !useCase && (<div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+            <button onClick={upload} className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+                        >Upload</button>
+            <button onClick={download} className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+                        >Download</button>
+          </div>)
+        }
+        {
+          useCase?.useCase === 'upload' && (<button onClick={download} className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+          >Download</button>)
+        }
+        {
+          useCase?.useCase === 'download' && (<button onClick={upload} className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
+          >Upload</button>)
+        }
+      </div>
 
-        <button onClick={upload}>Upload</button>
-        <button onClick={download}>Download</button>
+      <div>
         {
           useCase?.useCase === 'upload' && (<FileUploadComponant uploadGateway={di.uploadGateway}/>)
         }
-
-        
         {
-          fileNames.map(fileName => <FileDownloadComponant downloadGateway={di.downloadGateway} fileName={fileName}/>)
+          useCase?.useCase === 'download' && 
+              fileNames.map(fileName => <FileDownloadComponant downloadGateway={di.downloadGateway} fileName={fileName}/>)
         }
-        
       </div>
 
     </main>
