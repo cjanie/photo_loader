144e014465a110b219c9db6666b868c1881868e6
cjanie <janiechun@hotmail.fr>
2024-02-19 21:52:51 +0100

    use constant Storage directory

--- photo_loader/app/firebase/firebaseDownloadAdapter.ts
+++ photo_loader/app/firebase/firebaseDownloadAdapter.ts
@@ -1,8 +1,8 @@
 import { getDownloadURL, ref } from "firebase/storage";
 import { DownloadGateway } from "../gateways/DownloadGateway";
-import { storage } from "./firebase-config";
+import { STORAGE_DIRECTORY, storage } from "./firebase-config";
 import { fileNameQuery } from "./db/fileNameQuery";
 
 export const firebaseDownloadAdapter: DownloadGateway = {
-    getUrl: async (fileName: string) => getDownloadURL(ref(storage, 'files/' + fileName))
+    getUrl: async (fileName: string) => getDownloadURL(ref(storage, STORAGE_DIRECTORY +'/' + fileName))
 } 
