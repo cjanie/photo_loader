0d52a36987abd3e40621ab8c13c089b60056c318
cjanie <janiechun@hotmail.fr>
2024-02-21 20:55:49 +0100

    image componant

--- photo_loader/app/componants/FileUploadComponant.tsx
+++ photo_loader/app/componants/FileUploadComponant.tsx
@@ -50,7 +50,7 @@ export default function FileUploadComponant(props: FileUpload) {
                   file && 
                   (<div>
                     <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-                      <ImageComponant src={URL.createObjectURL(file)} alt={"preview image " + file.name}/>
+                      <ImageComponant src={URL.createObjectURL(file)} alt={"preview image " + file.name} size={700}/>
                     </div>
                     <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
                       <button className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30"
