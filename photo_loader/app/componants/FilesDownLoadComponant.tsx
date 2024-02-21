4aa66b254d5aa2e325cdf489150d9bb597162105
cjanie <janiechun@hotmail.fr>
2024-02-21 21:28:22 +0100

    arrow next

--- photo_loader/app/componants/FilesDownLoadComponant.tsx
+++ photo_loader/app/componants/FilesDownLoadComponant.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import FileDownloadComponant from "./FileDownloadComponant";
 import { DownloadGateway } from "../gateways/DownloadGateway";
+import ImageComponant from "./ImageComponant";
+import Image from "next/image";
 
 interface FilesDownLoad {
     downloadGateway: DownloadGateway,
@@ -39,7 +41,7 @@ export default function FilesDownloadComponant(props: FilesDownLoad) {
     }
 
     return (
-       <div>
+       <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
         {
             fileNames.map(fileName => fileName && <FileDownloadComponant 
                 key={fileName} 
@@ -48,7 +50,8 @@ export default function FilesDownloadComponant(props: FilesDownLoad) {
                 fileName={fileName}
                 imageSize={props.imageSize}/>)
         }
-        <button onClick={onClickNext}>Next</button>
+        <button onClick={onClickNext}><Image src="/icons/right-arrow-backup-2-svgrepo-com.svg" alt="arrow" height={50} width={50}/></button>
+        
        </div>
     )
 }
--- photo_loader/app/page.tsx
+++ photo_loader/app/page.tsx
@@ -6,6 +6,9 @@ export default function Home() {
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+
+      </div>
 
       <MenuComponant uploadGateway={di.uploadGateway} downloadGateway={di.downloadGateway} fileRefQueryGateway={di.fileRefQueryGateway}/>
 
