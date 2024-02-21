91967db7df944081f68f6a48786d22f065ee5222
cjanie <janiechun@hotmail.fr>
2024-02-21 23:56:48 +0100

    nav between photos

--- photo_loader/app/componants/FilesDownLoadComponant.tsx
+++ photo_loader/app/componants/FilesDownLoadComponant.tsx
@@ -11,37 +11,64 @@ interface FilesDownLoad {
     imageSize: number
 }
 
+interface DownloadPage {
+    fileNames: string[]
+}
+
 export default function FilesDownloadComponant(props: FilesDownLoad) {
 
+    const [pages, setPages] = useState<DownloadPage[]>([])
+
     const [fileNames, setFileNames] = useState<string[]>([])
-    const [nextPageIndex, setNextPageIndex] = useState<number>(0) 
+    
+    const [currentPageIndex, setCurrentPageIndex] = useState<number>(-1)
 
     useEffect(()=> {
-        if(nextPageIndex == 0) {
+        if(pages.length == 0) {
             initQuery()
         } else {
-            continueQuery()
+            if(currentPageIndex > -1 && currentPageIndex < pages.length) {
+                const backPage: DownloadPage = pages[currentPageIndex]
+                setFileNames(backPage.fileNames)
+            } else {
+                continueQuery()
+            }
+            
+            
+            
+            
         }
-    }, [nextPageIndex])
+    }, [currentPageIndex])
 
     const initQuery = () => {
         props.fileRefQueryGateway.initPageTokenQuery(props.subDirectoryName).then((pageToken => {
             setFileNames(pageToken.filesNames)
+            const firstPage: DownloadPage = {fileNames: pageToken.filesNames}
+            setPages([firstPage])
         }))
     }
   
     const continueQuery = () => {
         props.fileRefQueryGateway.nextPageToken().then(((pageToken) => {
             setFileNames(pageToken.filesNames)
+            const nextPage: DownloadPage = {fileNames: pageToken.filesNames}
+            const allPages = pages
+            allPages.push(nextPage)
+            setPages(allPages)
         }))
     }
 
     const onClickNext = () => {
-        setNextPageIndex(nextPageIndex + 1)
+        setCurrentPageIndex(currentPageIndex + 1)
+    }
+
+    const onClickBack = () => {
+        setCurrentPageIndex(currentPageIndex - 1)
     }
 
     return (
        <div className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+        <button onClick={onClickBack}>Back</button>
         {
             fileNames.map(fileName => fileName && <FileDownloadComponant 
                 key={fileName} 
